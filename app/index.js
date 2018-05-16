1bd8b96f36e970c4fdffb3969356b9b9b5ca518c
Romain Foucher <zarnes@live.com>
2018-05-16 18:45:26 +0200

    Created structure

---
+++ app/index.js
@@ -0,0 +1,75 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let fs = require('fs');
+
+app.get('*.js', function(req, res, next) {
+    console.log('loading js file : .' + req.url);
+    fs.readFile('.' + req.url.toString(), function(err, data) {
+        if (err)
+        {
+            console.log(err);
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Error reading file');
+        }
+        else
+        {
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            res.end(data);
+        }
+    })
+});
+
+app.get('*.css', function(req, res, next) {
+    console.log('loading css file : .' + req.url);
+    fs.readFile('.' + req.url.toString(), function(err, data) {
+        if (err)
+        {
+            console.log(err);
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Error reading file');
+        }
+        else
+        {
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.end(data);
+        }
+    });
+});
+
+app.get('/', function(req, res){
+    console.log("logging");
+    fs.readFile('./login.html', function(err, data){
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Error reading login page');
+        }
+        else
+        {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
+});
+
+app.post('/login', function(req, res){
+    // TODO add login
+})
+
+app.get('/lobby', function(req, res) {
+    console.log("lobby");
+    // TODO check login
+
+    fs.readFile('./lobby.html', function(err, data){
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Error reading login page');
+        }
+        else
+        {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    })
+});
+
+http.listen(80);
---
+++ app/lobby.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el:"#app",
+    data: {
+        pseudo: 'Zarnes',
+        players: [
+            {pseudo: 'Zarnes', ladder: '1', score: '1000', connected: 'true'},
+            {pseudo: 'Senraz', ladder: '2', score: '999', connected: 'true'},
+        ]
+    }
+});
+
+function showLadder(show) {
+
+}
